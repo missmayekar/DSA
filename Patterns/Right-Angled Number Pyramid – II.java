b55fb60214f4acffad584cb778fc8d61a5bb33c2
missmayekar <nehamayekar1999@gmail.com>
2023-06-01 16:54:26 +0530

    Right-Angled Number Pyramid – II.java

--- Patterns/Right-Angled Number Pyramid – II.java
+++ Patterns/Right-Angled Number Pyramid – II.java
@@ -7,9 +7,9 @@
 */
 
 public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j <= i; j++){
-                System.out.print(i+1);
+        for(int i = 1; i <= 5; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(i);
             }
             System.out.println();
         }
